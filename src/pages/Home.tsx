364371a0329b5ffbfcaced5b41e5d4bbaaf7ef40
Satosh-J <smartdev.house108@gmail.com>
2022-10-23 18:42:04 -0300

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home: FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Google map integration with react.
         </p>
         <a
           className="App-link"
