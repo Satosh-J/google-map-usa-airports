a1f802df8745f235151023ac73190e53537e94e5
Satosh-J <smartdev.house108@gmail.com>
2022-10-24 13:58:01 -0300

    update node server port set

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('./build'));
 
